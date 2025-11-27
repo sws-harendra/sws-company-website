912d4d7652b76b81de5202c537fe34128e78fa62
anand <anand.k4756@gmail.com>
2025-11-27 13:08:54 +0530

    added gtag

--- startup-web-support-main/app/layout.jsx
+++ startup-web-support-main/app/layout.jsx
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +25,20 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11559226606"
+        />
+
+        <Script id="google-ads-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11559226606');
+          `}
+        </Script>
+
         {children}
         <Toaster />
       </body>

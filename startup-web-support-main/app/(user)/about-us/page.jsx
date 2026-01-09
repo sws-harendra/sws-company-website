c8a7f1970a147c843715e987efb5e84a83458e0a
Atul kumar <atulvardhane128@gmail.com>
2026-01-09 17:05:24 +0530

    text change

--- startup-web-support-main/app/(user)/about-us/page.jsx
+++ startup-web-support-main/app/(user)/about-us/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <>
       <Seo
-        title="About Us | Startup Web Support – Leading IT Company in Patna"
+        title="About Us | Startup Web Support"
         description="Learn about Startup Web Support, a leading IT company in Patna offering website development, software solutions, digital marketing, SEO, and mobile app development services."
         canonical="https://startupwebsupport.com/about-us"
         image="sws-logo.png"

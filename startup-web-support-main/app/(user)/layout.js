534fff499622a10996c21c9923f91f55f6ac1f11
sws <appdevelopment.sws@gmail.com>
2026-01-13 13:17:07 +0530

    removed contact us

--- startup-web-support-main/app/(user)/layout.js
+++ startup-web-support-main/app/(user)/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
       <Call />
       <WhatsAppButton />
       <Header />
-      <ContactFormModal />
+      {/* <ContactFormModal /> */}
       {children}
       <Footer />
     </div>

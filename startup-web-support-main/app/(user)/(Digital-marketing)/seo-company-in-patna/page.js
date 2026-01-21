e9f85759103d323af1c78e15c5e1af4c93416485
sws <appdevelopment.sws@gmail.com>
2026-01-21 17:20:42 +0530

    removed unwanted title and description from meta tag

--- startup-web-support-main/app/(user)/(Digital-marketing)/seo-company-in-patna/page.js
+++ startup-web-support-main/app/(user)/(Digital-marketing)/seo-company-in-patna/page.js
@@ -6,13 +6,6 @@ import WhyBestSeo from "./components/WhyBestSeo";
 import SeoCta from "./components/SeoCta";
 import SeoFaq from "./components/SeoFaq";
 import Seo from "@/components/Seo";
-export const metadata = {
-  title: "SEO Company In Patna | Best SEO Services Agency In Patna",
-  description:
-    "Best SEO company in Patna delivering result-driven SEO services to boost website rankings, traffic, and online visibility for lasting business growth.",
-  keywords:
-    "Keyword:  seo company in patna, seo services in patna, Best SEO Company in Patna, seo in patna, seo agency in patna, top seo company in patna, best seo services in patna, seo company in bihar, seo company patna, seo services patna, seo agency patna, seo services in bihar, SEO Service in Patna, search engine optimization in patna, best seo company in bihar, best seo in patna, best seo company in India, best seo company, top seo company in india, top seo agency, web seo company, top seo agency in india, best seo service provider in india, best search engine optimization company, best search engine optimization company in india",
-};
 
 const page = () => {
   return (

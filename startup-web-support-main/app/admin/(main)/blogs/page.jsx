780b591bb70e8ab41d668f2df57bb3238b598b0f
sws <appdevelopment.sws@gmail.com>
2026-06-26 17:38:13 +0530

    width increased of blog form

--- startup-web-support-main/app/admin/(main)/blogs/page.jsx
+++ startup-web-support-main/app/admin/(main)/blogs/page.jsx
@@ -57,6 +57,7 @@ export default function BlogsPage() {
         <ReusableModal
           title="Add New Blog"
           triggerLabel="+ New Blog"
+          maxWidth="max-w-full"
         >
           {({ close }) => (
             <BlogForm
